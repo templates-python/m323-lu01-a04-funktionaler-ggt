553142b4d4262b9edd71ba69ed69e4079fbeed18
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-25 10:44:50 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+def ggt(a, b):
+    pass
 
 
 if __name__ == '__main__':
-    calculate()
+    zahl1 = 56
+    zahl2 = 48
+    resultat = ggt(zahl1, zahl2)
+    print(f"Der GGT von {zahl1} und {zahl2} ist {resultat}")  # Ausgabe: Der GGT von 56 und 48 ist 8
--- main_test.py
+++ main_test.py
@@ -1,6 +1,9 @@
 import main
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_ggt():
+    assert main.ggt(56, 48) == 8
+    assert main.ggt(101, 103) == 1
+    assert main.ggt(60, 48) == 12
+    assert main.ggt(0, 5) == 5
+    assert main.ggt(5, 0) == 5
+    assert main.ggt(0, 0) == 0
