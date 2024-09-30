384addcb738a60c851676bee94964f4a0c617b20
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:02:06 +0200

    Ran pyink on branch main

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def ggt(a, b):
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     zahl1 = 56
     zahl2 = 48
     resultat = ggt(zahl1, zahl2)
-    print(f"Der GGT von {zahl1} und {zahl2} ist {resultat}")  # Ausgabe: Der GGT von 56 und 48 ist 8
+    print(
+        f"Der GGT von {zahl1} und {zahl2} ist {resultat}"
+    )  # Ausgabe: Der GGT von 56 und 48 ist 8
